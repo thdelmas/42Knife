17171f7c3b23d0b5070a592628d661e344d59bbc
Theophile DELMAS <thdelmas@e1r1p2.42.fr>
2021-04-15 11:03:00 +0200

    add: sgoinfre linker

---
+++ sgoinfre-downloads-linker.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+sgoinfre_location='/sgoinfre/goinfre'
+downloads_location="$HOME"'/Downloads'
+
+if [ ! "$USER" ]
+then
+	echo 'Please set USER Variable'
+	exit 1
+fi
+sgoinfre_user_location="$sgoinfre_location"'/Perso/'"$USER"
+
+echo 'Welcome '"$USER"' !'
+
+if [ ! -e "$sgoinfre_location" ]
+then
+	echo 'Sgoinfre not found 😿'
+	exit 1
+fi
+
+if [ ! -e "$sgoinfre_user_location" ]
+then
+	echo "$sgoinfre_user_location"' not found 😿'
+	echo "Creating $sgoinfre_user_location ..."
+	mkdir -v "$sgoinfre_user_location"
+	chmod -R 700 "$sgoinfre_user_location"
+fi
+
+if [ ! -L "$downloads_location" ]
+then
+	echo "$downloads_location is still a directory in your home"
+	mv -v "$downloads_location" "$sgoinfre_user_location/"
+	ln -s "$sgoinfre_user_location/Downloads" "$downloads_location"
+fi
+echo 'See you soon ☺️ '
